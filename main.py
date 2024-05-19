281040ab7fb1c697397ede05d7f1db0da185a5ae
gellyzxc <urasaftn@gmail.com>
2024-05-19 22:26:08 +0500

    naxui idite

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import requests
 import html
 from bs4 import BeautifulSoup
 import html_to_json
-from datetime import datetime 
+from datetime import datetime
 
 def getInfo(stop_id):
-    # busstop_id = 23800
-
     page = requests.get(f'https://m.gortransperm.ru/stoppoint-arrival-times/{stop_id}/')
 
     data = html.unescape(page.text)
@@ -33,8 +31,50 @@ def getInfo(stop_id):
 
     return result
 
+def getBuses():
+    page = requests.get(f'https://m.gortransperm.ru/routes-list/0/')
+    
+    data = html.unescape(page.text)
+
+    soup = BeautifulSoup(data, "html.parser")
+
+    data = soup.find_all('li')
+    if (data == []):
+        return []
+    
+    data = html_to_json.convert(str(data))
+
+    result = []
+    for item in data['li']:
+        result.append({
+            "name": item['a'][0]['_value'],
+            "link": item['a'][0]['_attributes']['href'].split('/')[2]
+        })
+    return result
+
+def getBusStops(bus_id):
+    page = requests.get(f'https://m.gortransperm.ru/route/{bus_id}/')
+    
+    data = html.unescape(page.text)
+
+    soup = BeautifulSoup(data, "html.parser")
+
+    data = soup.find_all('li')
+    if (data == []):
+        return []
+    
+    data = html_to_json.convert(str(data))
+
+    result = []
+    for item in data['li']:
+        result.append({
+            "name": item['a'][0]['_value'],
+            "link": item['a'][0]['_attributes']['href'].split('/')[3]
+        })
+    return result
 
 if __name__ == "__main__":
     stop_id = input('stop id: ')
-    res = getInfo(stop_id=stop_id)
-    print(res)
+
+    # res = getInfo(stop_id=stop_id)
+    # print(res)
--- server.py
+++ server.py
@@ -1,13 +1,48 @@
 import json
 from flask import Flask
-from main import getInfo
+from main import getInfo, getBuses, getBusStops
 
 app = Flask(__name__)
 
 
 @app.route('/timetable/<stop_id>/')
-def generate_password(stop_id):
+def get_by_stop(stop_id):
     data = getInfo(stop_id=stop_id)
+    
+    response = app.response_class(
+        response=json.dumps({
+            "version": '1.0 public beta',
+            "dev": "gellyzxc https://gekksume.t.me/",
+            "data": data
+        }),
+        status=200,
+        mimetype='application/json'
+
+    )
+
+    return response
+
+@app.route('/timetable/')
+def get_buses():
+    data = getBuses()
+    
+    response = app.response_class(
+        response=json.dumps({
+            "version": '1.0 public beta',
+            "dev": "gellyzxc https://gekksume.t.me/",
+            "data": data
+        }),
+        status=200,
+        mimetype='application/json'
+
+    )
+
+    return response
+
+
+@app.route('/timetable/bus/<bus_id>')
+def get_bus(bus_id):
+    data = getBusStops(bus_id)
     
     response = app.response_class(
         response=json.dumps({
